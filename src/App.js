af863bff96d1a9a75f6323121f38068c35703d2e
Skchouhan753 <104707355+Skchouhan753@users.noreply.github.com>
2022-11-13 18:03:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,177 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <div className="nav1">
+          <div><p>Why licious?</p><p>Download</p><img src="https://www.licious.in/img/rebranding/ios_app_icon.svg" /><img src="https://www.licious.in/img/rebranding/android_app_icon.svg" /></div>
+          <div><p>FSSC 22000 certification</p><p>About Us</p><p>Career@Licious</p><p>Contact Us</p></div>
+        </div>
+        <div className="nav2">
+          <img src="https://www.licious.in/img/rebranding/licious-logo.svg" /><img src="https://www.licious.in/img/rebranding/location_icon.svg" />
+          <div><p>Kolkata</p><h6>KOLKKATA West Bengal</h6></div>
+          <form>
+            <input type="search" placeholder="Search for any delicious product" />
+            <button type="submit">Search</button>
+          </form>
+          <img src="https://www.licious.in/img/rebranding/category-dropdown-icon.svg" />
+          <p>Categories</p>
+          <img src="https://www.licious.in/img/rebranding/profile_icon.svg" />
+          <p>Login</p>
+          <img src="https://www.licious.in/img/rebranding/cart_icon.svg" />
+          <p>Cart</p>
+        </div>
+      </div>
+      <img className="banner" src="https://d2407na1z3fc0t.cloudfront.net/Slider/banner_63630e9d6ba0e" />
+      <p className="shop">Shop by categories</p><p>Freshest meats just for you</p>
+
+      <div className="meats">
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/cb6e4eb8-6aec-7872-1638-0c2cf7970b71/original/Todays_Deals.png" /><p>Today's Deals</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/34466dbd-a515-edd1-3e99-05000f217cb6/original/Chicken_(2).png" /><p>Chicken</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/caac432f-545f-f03f-ce10-3b911916da70/original/FIsh_(1).png" /><p>Fish & Seafood</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/3a3d173d-5537-dafc-0be4-dec0791dcd24/original/MUT.png" /><p>Mutton</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/21653c3a-4d6d-da71-2432-6833b88e9629/original/RC.png" /><p>Ready to Cook</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/f4053965-f199-80a0-2551-d85d712574e2/original/Prawn_(2).png" /><p>Prawns</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/49a8dd0c-7254-0b89-b348-b57281c76f5a/original/Coldcuts_(2).png" /><p>Cold Cuts</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/d9a97969-ebd7-977c-e676-b343a18d7318/original/SPD.png" /><p>Spreads</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/1bd08fae-c971-390a-ce8a-6f6502f5bd0d/original/Eggs_(1).png" /><p>Eggs</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/0b7ccd0f-0811-c38b-5420-0317c8006bda/original/Biryani_(2).png" /><p>Briyani & Kebab</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/69b72338-4180-2631-b175-04265b1e5c7a/original/Combo_(2).png" /><p>Combos</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/5329c23d-219d-c1aa-e044-d6d305da77f0/original/Gourmet.png" /><p>Gourmet</p></div>
+      </div>
+
+      <div className="joinNow">
+        <div><img src="https://www.licious.in/img/rebranding/loyalty_licious_logo.svg" /> <button>JOIN NOW</button><hr /><p>Join MEATOPIA to get free delivery on all orders with cart value more than Rs.99.</p></div>
+      </div>
+
+      <div className="liciousWay">
+        <div>Premium<br />Produce</div>
+        <div>World-Class Central<br />Production Unit</div>
+        <div>150 Quality<br />Checks</div>
+        <div>Delivered Fresh<br />Everyday</div>
+        <div>Extraordinary<br />Cooking</div>
+      </div>
+      <div className="ads">
+        <img src="https://dao54xqhg9jfa.cloudfront.net/OMS-StaticBanner/4c895baf-28fb-2833-8d19-37d05c51d39c/original/static-bank-units-Sep-web_(11).jpg?format=webp" />
+      </div>
+      <p className="sell">Best Sellers</p>
+      <div className="fooditem">
+
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/763459f8-7d85-8165-d303-dbc29dd51f8e/original/kkkkkkkkkk.jpg?format=webp" />
+          <p>Chicken Curry Cut - Small Pieces</p><p>Fresh Nakhre & fresh bone-in & boneless chicken cuts</p><p>500gms</p><br />
+          <span><p>MRP:185/-</p><button>ADD TO CART</button></span>
+        </div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/2ea78a8a-739e-258b-cdd9-82bb1a8d7938/original/large.jpg?format=webp" />
+          <p>Chicken Curry Cut - Large Pieces (Large Pack)</p><p>Half a chicken cut to chunky pieces including Two leg, a win</p><p>1000gms</p><br />
+          <span><p>MRP:355/-</p><button>ADD TO CART</button></span>
+        </div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/51d35fd5-855d-8d14-ae83-c55bc64e7aa5/original/Classic-Eggs---Pack-of-6--Hero-Shot.jpg?format=webp" />
+          <p>Classic Eggs - Pack Of 6</p><p>Pack of 6 naturally laid, white eggs from healthy hens</p><p>Pieces 6</p><br />
+          <span><p>MRP:75/-</p><button>ADD TO CART</button></span>
+        </div>
+      </div>
+      <p className="sell">Boneless Cuts</p>
+      <div className="fooditem">
+
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/79c5cbf9-4cd5-a03e-b42c-f0be00b12715/original/Goat-Boneless-Hero-Shot.jpg?format=webp" />
+          <p>Chicken Curry Cut - Small Pieces</p><p>Fresh Nakhre & fresh bone-in & boneless chicken cuts</p><p>500gms</p><br />
+          <span><p>MRP:929/-</p><button>ADD TO CART</button></span>
+
+        </div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/82a61ed7-be73-657d-5dce-fb5ba4204702/original/Chicken-Tag.jpg?format=webp" />
+          <p>Chicken Curry Cut - Large Pieces (Large Pack)</p><p>Half a chicken cut to chunky pieces including Two leg, a win</p><p>1000gms</p><br />
+          <span><p>MRP:299/-</p><button>ADD TO CART</button></span>
+        </div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/55af1bc7-bcd1-db41-cb56-7046410a2f63/original/p0_tile_images-07_(1).jpg?format=webp" />
+          <p>Classic Eggs - Pack Of 6</p><p>Pack of 6 naturally laid, white eggs from healthy hens</p><p>Pieces 6</p><br />
+          <span><p>MRP:429/-</p><button>ADD TO CART</button></span>
+        </div>
+      </div>
+      <div className="reward">
+        <div>
+          <img src="https://www.licious.in/img/rebranding/rewards/rewards_icon.svg" /><p>My Rewards<br />Place an order and win your next reward!</p><button>CLICK TO VIEW</button>
+        </div>
+      </div>
+      <p className="sell">Explore by category</p>
+      <div className="meats">
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/cb6e4eb8-6aec-7872-1638-0c2cf7970b71/original/Todays_Deals.png" /><p>Today's Deals</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/34466dbd-a515-edd1-3e99-05000f217cb6/original/Chicken_(2).png" /><p>Chicken</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/caac432f-545f-f03f-ce10-3b911916da70/original/FIsh_(1).png" /><p>Fish & Seafood</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/3a3d173d-5537-dafc-0be4-dec0791dcd24/original/MUT.png" /><p>Mutton</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/21653c3a-4d6d-da71-2432-6833b88e9629/original/RC.png" /><p>Ready to Cook</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/f4053965-f199-80a0-2551-d85d712574e2/original/Prawn_(2).png" /><p>Prawns</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/49a8dd0c-7254-0b89-b348-b57281c76f5a/original/Coldcuts_(2).png" /><p>Cold Cuts</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/d9a97969-ebd7-977c-e676-b343a18d7318/original/SPD.png" /><p>Spreads</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/1bd08fae-c971-390a-ce8a-6f6502f5bd0d/original/Eggs_(1).png" /><p>Eggs</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/0b7ccd0f-0811-c38b-5420-0317c8006bda/original/Biryani_(2).png" /><p>Briyani & Kebab</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/69b72338-4180-2631-b175-04265b1e5c7a/original/Combo_(2).png" /><p>Combos</p></div>
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-Category/5329c23d-219d-c1aa-e044-d6d305da77f0/original/Gourmet.png" /><p>Gourmet</p></div>
+      </div>
+      <p className="sell">Breakfast & Snacking Specials</p>
+      <div className="fooditem">
+
+        <div><img src="https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/f9de17db-1263-4d79-8600-477f07c24a6e/original/Chunky-Shawarma-Chicken-Spread_(7).jpg?format=webp" />
+          <p>Chunky Herby Tomato Chicken Spread</p><p>A Pleasent constrst of Meaty Chicken Chunks Spread</p><p>Pieces:1</p><br />
+          <span><p>MRP:219/-</p><button>ADD TO CART</button></span>
+
+        </div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_1puko17mted/34/prod_display_image/1634575678.9089--2021-10-1822:17:58--1818" />
+          <p>Chunky Continental Eggs Spreads</p><p>Creamy egg spread flavoured with garlic</p><p>Pieces:1</p><br />
+          <span><p>MRP:199/-</p><button>ADD TO CART</button></span>
+        </div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_drokbgb2jhb/34/prod_display_image/1634375275.424--2021-10-1614:37:55--905?format=webp" />
+          <p>Chunky Sriracha Prawn Spread</p><p>A blend of freshl cooked juicy prawn chunks</p><p>Pieces:1</p><br />
+          <span><p>MRP:149/-</p><button>ADD TO CART</button></span>
+        </div>
+      </div>
+      <p className="sell">Check out our blog</p>
+      <div className="fooditem">
+        <div><img src="https://www.licious.in/blog/wp-content/uploads/2022/11/shutterstock_559553158-1.jpg" />
+          <h4>All about Chicken Salami</h4>
+        </div>
+        <div><img src="https://www.licious.in/blog/wp-content/uploads/2022/11/shutterstock_1920454280.jpg" />
+          <h4>Sausages: Are Sausages Healthy? Health Benefits of Chicken Sausages for A Healthy You!</h4>
+        </div>
+        <div><img src="https://www.licious.in/blog/wp-content/uploads/2022/10/blog-image-02.jpg" />
+          <h4>Make Anda Paratha Rolls Special With Licious Chicken Lyoners!</h4>
+        </div>
+      </div>
+      <div className="liciouisBox">
+        <img src="https://d2407na1z3fc0t.cloudfront.net/homepageStaticBanner/homepageStaticBanner_62a34bdbb6f63" />
+      </div>
+      <p className="sell">In the News</p>
+      <div className="news">
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/35/inthenews_62aadde78bdae" /><p>India’s Licious becomes unicorn with $52 million fresh fundraise</p></div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/37/inthenews_62aae0b741820" /><p>Why Licious wants to become the everything store for meat</p></div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/38/inthenews_62aae10540001" /><p>They slayed COVID, went on to build a unicorn: Meet the founders of Licious</p></div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/40/inthenews_62aae21e9a38e" /><p>Lucious founders on building a unicorn startups</p></div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/42/inthenews_62aae2a6d9bc4" /><p>Licious wants you to eat more meat</p></div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/43/inthenews_62aae2dadd8aa" /><p>Online meat startup Licious unveils ads featuring Anil Kapoor, Arjun Kapoor</p></div>
+        <div><img src="https://d2407na1z3fc0t.cloudfront.net/inTheNews/45/inthenews_62aae4c4d8331" /><p>'FasterChef' treats uninvited friends with Licious</p></div>
+      </div>
+      <div className="logo">
+        <img src="https://www.licious.in/img/rebranding/licious-logo.svg" />
+      </div>
+      <div className="footer">
+      <div className="footer1"><h4>USEFUL LINKS</h4><p>Why Licious?<br />Refer & Earn<br />Licious Cash & Cash+<br />Careers<br />BLOG<br />Campaign<br />Bug Bounty Guidelines<br />About Us<br />FSSC 22000 Certification<br />FSSAI Licenses<br />Sitemap</p></div>
+      <div className="footer1"><h4>EXPERIENCE LICIOUS APP ON MOBILE</h4><img src="https://www.licious.in/img/rebranding/app-store-new.svg"/><img src="https://www.licious.in/img/rebranding/play-store-new.png"/></div>
+      <div className="footer1"><h4>CONTACT US</h4><p>Call: 1800-4190-786<br/>Talktous@licious.com<br/>REGISTERED OFFICE ADDRESS:<br/>House of Licious, Zed Pearl, No 12,<br/>Domlur Layout<br/>Bangalore, Karnataka - 560071</p></div>
+      <div className="footer1"><h4>T&C</h4><p>FAQ<br/>Privacy Policy<br/>HAVE SECURITY CONCERN?<br/>Mail us to: security@licious.com</p></div>
+      <div className="footer1"><img className="card"src="https://www.licious.in/img/rebranding/3-dsecure.png"/></div>
+      </div>
+      <div className="footer2">
+       <div>
+       <img src="https://www.licious.in/img/rebranding/copyright2.png"/><h5>2022 Delightful Gourmet Pvt Ltd. All Rights Reserved.</h5>
+       </div> 
+       <p>Licious is your one-stop fresh meat delivery shop. In here, you get nothing but the freshest meat & seafood, delivered straight to your doorstep. Now you can buy meat online anytime at your convenience. Indulge in our diverse selection: Chicken, Mutton, Seafood (Fish, Prawns, Crabs), Marinades & Cold Cuts. All our products are completely natural and healthy. Once you've experienced Licious, you'll never go back to the old way of buying meat and seafood.</p>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+export default App;
